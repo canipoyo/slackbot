a225a1b32fc2e4f2fe78dbfaf2f3b207373d6951
canipoyo <39935426+canipoyo@users.noreply.github.com>
2019-01-25 11:14:08 +0900

    Add files via upload

---
+++ iku_git.rb
@@ -0,0 +1,9 @@
+require 'slack-ruby-client'
+
+Slack.configure do |conf|
+conf.token = ENV['SLACK_API_TOKEN']
+end
+client = Slack::Web::Client.new
+loop do
+    client.chat_postMessage(channel: 'bottest', text: "イク\n", as_user: true)
+end
