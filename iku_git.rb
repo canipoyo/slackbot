ceb299d4715d2f9163c206670e0ac8c2336b6c67
Nerun <39396637+neruneru0419@users.noreply.github.com>
2019-01-27 12:44:44 +0900

    Update iku_git.rb

--- iku_git.rb
+++ iku_git.rb
@@ -1,9 +1,10 @@
 require 'slack-ruby-client'
 
 Slack.configure do |conf|
-conf.token = "xoxb-445928392119-532985027941-de921tJ3NpP5pzD1cMZIWU5y"
+conf.token =  "xoxb-445928392119-534444550214-mq2TA0ZuimPImAUaKa3KJRlO"
 end
 client = Slack::Web::Client.new
+
 loop do
-    client.chat_postMessage(channel: 'bottest', text: "イク\n", as_user: true)
+    client.chat_postMessage(channel: 'bottest', text: "イク", as_user: true)
 end
