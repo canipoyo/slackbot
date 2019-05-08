93e6a4f0dd7300942b521ead9cc74cd57e35524e
Nerun <39396637+neruneru0419@users.noreply.github.com>
2019-05-08 13:16:05 +0900

    Update iku_git.rb

--- iku_git.rb
+++ iku_git.rb
@@ -4,7 +4,7 @@ Slack.configure do |conf|
 conf.token =  "xoxb-445928392119-534444550214-V3VCau0EQUq1zXQH7H4p8Mkr"
 end
 client = Slack::Web::Client.new
-
-
+loop do
     client.chat_postMessage(channel: 'bottest', text: "イク", as_user: true)
-
+    sleep(1)
+end
