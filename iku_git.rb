7bb6358635fb077d3cd175fe0b14d3042dd7d906
canipoyo <39935426+canipoyo@users.noreply.github.com>
2019-05-08 13:25:40 +0900

    Update iku_git.rb
    sex

--- iku_git.rb
+++ iku_git.rb
@@ -1,7 +1,8 @@
 require 'slack-ruby-client'
 
 Slack.configure do |conf|
-conf.token =  "xoxb-445928392119-534444550214-V3VCau0EQUq1zXQH7H4p8Mkr"
+#conf.token =  "xoxb-445928392119-534444550214-V3VCau0EQUq1zXQH7H4p8Mkr"
+ conf.token =  "xoxs-445928392119-628726913844-622913163217-ca6d0abd76924949b45d325f5cd07d49fa0bd037de79d1558da785ca2b08eb1d"
 end
 client = Slack::Web::Client.new
 loop do
